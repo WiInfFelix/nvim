226ef414624fbc8f4b63f1f023eff6afc7a294ab
Felix Scheffler <schefflerfelix@protonmail.com>
2024-05-28 08:14:06 +0200

    added go tester/debugger and ui changes

---
+++ lua/felix/plugins/neotest.lua
@@ -0,0 +1,19 @@
+return {
+	{ "nvim-neotest/nvim-nio" },
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"fredrikaverpil/neotest-golang", -- Installation
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-golang"), -- Registration
+				},
+			})
+		end,
+	},
+}
